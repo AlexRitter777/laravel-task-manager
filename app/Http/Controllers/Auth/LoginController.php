79be5bcf0ce896f82e338c19a8053c337a6b6a52
Alex <bogachev@email.cz>
2026-05-03 18:41:00 +0200

    Oops..

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -33,7 +33,7 @@ class LoginController extends Controller
 
     public function destroy(Request $request){
         Auth::logout();
-        $request->session()->regenerate();
+        $request->session()->invalidate();
         $request->session()->regenerateToken();
 
         return redirect()->route('login');
